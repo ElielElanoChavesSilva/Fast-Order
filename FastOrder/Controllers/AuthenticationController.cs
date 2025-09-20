b330e7c323a85287595a7e8e4c1921dcfad9d022
Eliel Elano Chaves Silva <elielelano7@gmail.com>
2025-09-19 23:08:54 -0300

    feat: Criando o Controller de Authentication

---
+++ FastOrder/Controllers/AuthenticationController.cs
@@ -0,0 +1,16 @@
+﻿using FastOrder.Application.Contracts;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FastOrder.Controllers
+{
+    [Route("auth")]
+    public class AuthenticationController : BaseController
+    {
+        private readonly IAuthenticationService _authenticationService;
+
+        public AuthenticationController(IAuthenticationService authenticationService)
+        {
+            _authenticationService = authenticationService;
+        }
+    }
+}
