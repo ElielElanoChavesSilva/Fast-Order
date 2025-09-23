7fb04c9049578fffd24835d125b31db1a08967fb
Eliel Elano Chaves Silva <elielelano7@gmail.com>
2025-09-22 22:39:43 -0300

    feat: Criando o método add de Client

--- FastOrder/Controllers/ClientController.cs
+++ FastOrder/Controllers/ClientController.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace FastOrder.Controllers
 {
+    [Route("clients")]
     public class ClientController : BaseController
     {
         private readonly IClientService _clientService;
@@ -12,6 +13,34 @@ namespace FastOrder.Controllers
         {
             _clientService = clientService;
         }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ClientDTO>> FindByIdAsync(long id)
+        {
+            return await _clientService.FindById(id);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<ClientDTO>> PostAsync()
+        {
+            return await _clientService.Add();
+        }
+
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<ClientDTO>> PutAsync(long id)
+        {
+            return await _clientService.Update(id);
+        }
+
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<ClientDTO>> Delete(long id)
+        {
+            return await _clientService.FindById(id);
+        }
+
+
+        [HttpGet("{id}")]
         public async Task<ActionResult<ClientDTO>> FindById(long id)
         {
             return await _clientService.FindById(id);
