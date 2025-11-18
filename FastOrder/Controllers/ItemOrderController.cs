f288dccdc0ed383d8685d33e2bd669dcb351edc8
Eliel Elano Chaves Silva <elielelano7@gmail.com>
2025-11-17 22:20:05 -0300

    feat: Criando o controller de ItemOrder

--- FastOrder/Controllers/ItemOrderController.cs
+++ FastOrder/Controllers/ItemOrderController.cs
@@ -1,9 +1,37 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FastOrder.Application.Contracts;
+using FastOrder.Application.DTOs.Order.ItemOrder;
+using Microsoft.AspNetCore.Mvc;
 
 namespace FastOrder.Controllers
 {
-    public class ItemOrderController : BaseController
+
+    [Route("orders/{idOrder}/items")]
+    public class ItemOrderController(IItemOrderService itemOrderService) : BaseController
     {
-      
+        [HttpPost]
+        public async Task<CreatedResult> PostAsync(long orderId, ItemOrderPostDTO dto)
+        {
+            var id = await itemOrderService.PostItemOrderAsync(dto, orderId);
+            return Created($"orders/{orderId}/items/{id}", id);
+        }
+
+        [HttpGet("{itemId}")]
+        public async Task<ActionResult<ItemOrderResponseDTO>> GetByIdAsync(long itemId)
+        {
+            return Ok(await itemOrderService.FindByIdAsync(itemId));
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ItemOrderResponseDTO>>> GetAllAsync()
+        {
+            return Ok(await itemOrderService.FindAllAsync());
+        }
+
+        [HttpDelete("{itemId}")]
+        public IActionResult DeleteAsync(long itemId)
+        {
+            itemOrderService.Delete(itemId);
+            return NoContent();
+        }
     }
 }
